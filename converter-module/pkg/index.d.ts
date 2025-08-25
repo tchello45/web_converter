449920c7acd334f614f34b12893c1e8cbd344ca6
Tilman Kurmayer <tilman.kurmayer@gmail.com>
2025-08-25 14:06:45 +0200

    ci(module): :green_heart: Include the compiled package to prevent the need for building on vercel

---
+++ converter-module/pkg/index.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export function convert_text_to_binary(text: string): string;
+export function convert_binary_to_text(binary: string): string;
+export function convert_text_to_hex(text: string): string;
+export function convert_hex_to_text(hex: string): string;
+export function convert_binary_to_hex(binary: string): string;
+export function convert_hex_to_binary(hex: string): string;
---
+++ converter-module/pkg/index_bg.wasm.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const convert_text_to_binary: (a: number, b: number) => [number, number];
+export const convert_binary_to_text: (a: number, b: number) => [number, number];
+export const convert_text_to_hex: (a: number, b: number) => [number, number];
+export const convert_hex_to_text: (a: number, b: number) => [number, number];
+export const convert_binary_to_hex: (a: number, b: number) => [number, number];
+export const convert_hex_to_binary: (a: number, b: number) => [number, number];
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
