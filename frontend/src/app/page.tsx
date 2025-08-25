b97c5e1be2dc0b9bbfe6d9fd71639d6192bbb71c
Tilman Kurmayer <tilman.kurmayer@gmail.com>
2025-08-25 12:28:50 +0200

    feat(frontend): :sparkles: add Card component layout for text conversion

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,14 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -7,6 +18,13 @@ export default function Home() {
           Simple, fast and reliable conversion between text, binary and hex.
         </p>
       </section>
+      <Card className="flex-1 w-3/4 mt-24">
+        <CardContent className="flex-1 flex justify-between gap-40 m-10">
+
+          <Textarea placeholder="Enter text to convert" className="resize-none" />
+          <Textarea placeholder="Result" className="resize-none" disabled />
+        </CardContent>
+      </Card>
     </main>
   );
 }
